8498baf652349252772bd00d59b73a23944c4bda
Denislav Ivanov <denislavivanov82@gmail.com>
2024-09-24 09:33:29 +0300

    Fix variable names

--- main.c
+++ main.c
@@ -10,13 +10,13 @@
 #define BUFFER_SIZ 1024
 #define TOTAL_SIZ  2048
 
-struct mail_client
+struct SMTP_Client
 {
-    char* buff;
-    char* rcpt;
-    int   sock;
-    int   rcpt_cnt;
-    char  domain[64];
+    char* Buff;
+    char* Rcpt;
+    int   Sock;
+    int   RcptCnt;
+    char  Domain[64];
 };
 
 in_addr_t get_mail_server(const char* domain)
@@ -106,20 +106,20 @@ _Bool smtp_msg_complete(const char* msg)
     return *line_begin == '\0' && status == ' ';
 }
 
-void smtp_get_response(struct mail_client* client,
+void smtp_get_response(struct SMTP_Client* client,
                        int* response_code)
 {
     do
     {
-        smtp_recv(client->sock, client->buff, BUFFER_SIZ);   
-        printf("%s", client->buff);
+        smtp_recv(client->Sock, client->Buff, BUFFER_SIZ);   
+        printf("%s", client->Buff);
     } 
-    while (!smtp_msg_complete(client->buff));
+    while (!smtp_msg_complete(client->Buff));
 
-    sscanf(client->buff, "%d", response_code);
+    sscanf(client->Buff, "%d", response_code);
 }
 
-void smtp_handshake(struct mail_client* client)
+void smtp_handshake(struct SMTP_Client* client)
 {
     int response_code;
 
@@ -128,36 +128,36 @@ void smtp_handshake(struct mail_client* client)
 
     if (response_code != 220)
     {
-        smtp_send(client->sock, "QUIT\r\n", 6);
+        smtp_send(client->Sock, "QUIT\r\n", 6);
         fprintf(stderr, "* SMTP handshake fail! Code: %d\n", response_code);
         return;
     }
 
-    smtp_send(client->sock, "EHLO ", 5);
-    smtp_send(client->sock, client->domain, strlen(client->domain));
+    smtp_send(client->Sock, "EHLO ", 5);
+    smtp_send(client->Sock, client->Domain, strlen(client->Domain));
 
     /* Server options */
     smtp_get_response(client, &response_code);
 }
 
-void smtp_sender(struct mail_client* client, 
+void smtp_sender(struct SMTP_Client* client, 
                  const char* sender)
 {
     int response_code;
 
-    sprintf(client->buff, "MAIL FROM:<%s>\r\n", sender);
+    sprintf(client->Buff, "MAIL FROM:<%s>\r\n", sender);
 
-    smtp_send(client->sock, client->buff, strlen(client->buff));
+    smtp_send(client->Sock, client->Buff, strlen(client->Buff));
     smtp_get_response(client, &response_code);
 
     if (response_code != 250)
     {
         fprintf(stderr, "* SMTP sender fail:\n");
-        fprintf(stderr, "%s\n", client->buff);
+        fprintf(stderr, "%s\n", client->Buff);
     }
 }
 
-void smtp_recipients(struct mail_client* client,
+void smtp_recipients(struct SMTP_Client* client,
                      const char* recipients, int cnt)
 {
     int    i;
@@ -168,14 +168,14 @@ void smtp_recipients(struct mail_client* client,
     {
         recipient_len = strlen(recipients) + 1;
 
-        sprintf(client->buff, "RCPT TO:<%s>\r\n", recipients);
-        smtp_send(client->sock, client->buff, recipient_len + 11);
+        sprintf(client->Buff, "RCPT TO:<%s>\r\n", recipients);
+        smtp_send(client->Sock, client->Buff, recipient_len + 11);
         smtp_get_response(client, &response_code);
 
         if (response_code != 250)
         {
             fprintf(stderr, "* SMTP recipients fail:\n");
-            fprintf(stderr, "%s\n", client->buff);
+            fprintf(stderr, "%s\n", client->Buff);
 
             return;
         }
@@ -184,32 +184,32 @@ void smtp_recipients(struct mail_client* client,
     }
 }
 
-void smtp_body(struct mail_client* client, char* body)
+void smtp_body(struct SMTP_Client* client, char* body)
 {
     int response_code;
 
-    smtp_send(client->sock, "DATA\r\n", 6);
+    smtp_send(client->Sock, "DATA\r\n", 6);
     smtp_get_response(client, &response_code);
 
     if (response_code != 354)
     {
         fprintf(stderr, "* SMTP fail:\n");
-        fprintf(stderr, "%s\n", client->buff);
+        fprintf(stderr, "%s\n", client->Buff);
 
         return;
     }
 
-    smtp_send(client->sock, body, strlen(body));
-    smtp_send(client->sock, ".\r\n", 3);
+    smtp_send(client->Sock, body, strlen(body));
+    smtp_send(client->Sock, ".\r\n", 3);
     smtp_get_response(client, &response_code);
 
     if (response_code != 250)
     {
         fprintf(stderr, "* SMTP fail:\n");
-        fprintf(stderr, "%s\n", client->buff);
+        fprintf(stderr, "%s\n", client->Buff);
     }
 
-    smtp_send(client->sock, "QUIT\r\n", 6);
+    smtp_send(client->Sock, "QUIT\r\n", 6);
 }
 
 void smtp_build_body(char* dst, const char* subject)
@@ -217,31 +217,31 @@ void smtp_build_body(char* dst, const char* subject)
 
 }
 
-void get_recipients(struct mail_client* client)
+void get_recipients(struct SMTP_Client* client)
 {
     size_t len;
     size_t max = 1024;
     size_t offset = 0;
 
-    client->rcpt_cnt = 0;
+    client->RcptCnt = 0;
 
     printf("Recipient(s):\n");
 
     do
     {
         printf("-> ");
-        fgets(&client->rcpt[offset], max, stdin);
-        len = strlen(&client->rcpt[offset]);
+        fgets(&client->Rcpt[offset], max, stdin);
+        len = strlen(&client->Rcpt[offset]);
 
         max    -= len;
         offset += len;
-        ++client->rcpt_cnt;
-        client->rcpt[offset - 1] = '\0';
+        ++client->RcptCnt;
+        client->Rcpt[offset - 1] = '\0';
     }
     while (len > 1 && max > 0);
 
     /* Remove extra record */
-    --client->rcpt_cnt;
+    --client->RcptCnt;
 }
 
 int main(int argc, const char* argv[])
@@ -254,31 +254,31 @@ int main(int argc, const char* argv[])
 
     int    sock;
     struct sockaddr_in dest;
-    struct mail_client client;
+    struct SMTP_Client client;
 
     dest.sin_family      = AF_INET;
     dest.sin_port        = htons(25);
     dest.sin_addr.s_addr = get_mail_server(argv[1]);
 
-    client.sock     = socket(AF_INET, SOCK_STREAM, 0);
-    client.buff     = malloc(TOTAL_SIZ);
-    client.rcpt     = client.buff + BUFFER_SIZ;
-    strncpy(client.domain, "example.org\r\n", 64);
+    client.Sock = socket(AF_INET, SOCK_STREAM, 0);
+    client.Buff = malloc(TOTAL_SIZ);
+    client.Rcpt = client.buff + BUFFER_SIZ; 
+    strncpy(client.Domain, "example.org\r\n", 64);
 
-    assert(client.sock != -1);
-    assert(client.buff != NULL);
+    assert(client.Sock != -1);
+    assert(client.Buff != NULL);
 
     get_recipients(&client);
 
     uint64_t visited;
 
-    char*  p = client.rcpt;
+    char*  p = client.Rcpt;
     char*  q;
     char*  domain;
     char*  curr_domain;
     size_t len;
 
-    for (size_t i = 0; i < client.rcpt_cnt; ++i)
+    for (size_t i = 0; i < client.RcptCnt; ++i)
     {
         len = strlen(p) + 1;
 
@@ -287,7 +287,7 @@ int main(int argc, const char* argv[])
             curr_domain = strchr(p, '@') + 1;
             q = p;
 
-            for (size_t j = i; j < client.rcpt_cnt; ++j)
+            for (size_t j = i; j < client.RcptCnt; ++j)
             {
                 domain = strchr(q, '@') + 1;
 
